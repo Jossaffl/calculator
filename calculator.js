b15c830453ebe6d6c0fcdcec45dafa06bcc38536
Jossaffl <116684778+Jossaffl@users.noreply.github.com>
2023-02-14 01:47:33 -0500

    basic calculator, missing dot functionality

--- calculator.js
+++ calculator.js
@@ -8,6 +8,9 @@ plus.value = "+"
 minus = document.getElementById("subt")
 minus.value = "-"
 
+divider = document.getElementById("divd")
+divider.value = "/"
+
 multiplier = document.getElementById("mult")
 multiplier.value = "*"
 
@@ -20,11 +23,12 @@ equal.value = "="
 ac = document.getElementById("ac")
 
 numbers_container = document.getElementById("numbers")
+
 numbers = document.getElementsByClassName("number_buttons")
 screen.textContent = 0
 
 
-for (i=9; i >= 0; i--){
+for (i=1; i <= 9; i++){
     number = document.createElement("button")
     number.textContent = i
     number.value = i
@@ -32,65 +36,110 @@ for (i=9; i >= 0; i--){
     numbers_container.appendChild(number)   
 }
 
+zero = document.getElementById("zero")
+zero.value = 0
+zero.classList.add("number_buttons")
+
+
 
 
 function sum(a,b){
     screen.textContent = (+a) + (+b)
+    screen_arr["1"] = screen.textContent
+    screen_arr["operator"] = null
+    screen_arr["2"] = null
+
 }
 
 function subt(a,b){
     screen.textContent = (+a) - (+b)
+    screen_arr["1"] = screen.textContent
+    screen_arr["operator"] = null
+    screen_arr["2"] = null
 }
 
 function multiply(a,b){
     screen.textContent = (+a) * (+b)
+    screen_arr["1"] = screen.textContent
+    screen_arr["operator"] = null
+    screen_arr["2"] = null
 }
 
 function divide(a,b){
+    if(a == 0 || b == 0 ){
+        alert("You can't divide by 0!!")
+    }
+    else{
     screen.textContent = (+a) / (+b)
-
+    screen_arr["1"] = screen.textContent
+    screen_arr["operator"] = null
+    screen_arr["2"] = null}
 }
-
 
 Array.from(numbers).forEach((element) => {           // writes numbers on screen and replaces 0 if only value                                                                   
     element.addEventListener("click", (e)=>{
 
         if (screen.textContent == 0){
             screen.textContent = element.value
+       
+        } else if(screen_arr["operator"]!= null && screen_arr["1"]!= null){
+            screen.textContent = element.value
 
         }else{
             screen.textContent += element.value
         }})})
 
-Array.from(operators).forEach((element) => {          // clicking operators will store the first number and operator and reset screen to 0
-    element.addEventListener("click", () => {
-        screen_arr["1"] = screen.textContent
-        screen_arr["operator"] = element.value
-        screen.textContent = 0
 
-    })
-})
 
-equal.addEventListener("click", () => {               //provides result 
+function result(){
+
     screen_arr["2"] = screen.textContent
-    if (screen_arr["operator"] == "+"){
-        sum(screen_arr["1"], screen_arr["2"])
 
-    }else if (screen_arr["operator"] == "-"){
-        subt(screen_arr["1"], screen_arr["2"])
+        if (screen_arr["operator"] == "+"){
+            sum(screen_arr["1"], screen_arr["2"])
 
-    }else if (screen_arr["operator"] == "*"){
-        multiply(screen_arr["1"], screen_arr["2"])
+        }else if (screen_arr["operator"] == "-"){
+            subt(screen_arr["1"], screen_arr["2"])
 
-    }else if (screen_arr["operator"] == "/"){
-        divide(screen_arr["1"], screen_arr["2"])
+        }else if (screen_arr["operator"] == "*"){
+            multiply(screen_arr["1"], screen_arr["2"])
+
+        }else if (screen_arr["operator"] == "/"){
+            divide(screen_arr["1"], screen_arr["2"])
+        
+        }else{ 
+            console.log("something went wrong")}}
+
+
+Array.from(operators).forEach((element) => {        // clicking operators will store the first number and operator and reset screen to 0
+    element.addEventListener("click", () => {
+        if (screen_arr["1"] == null){
+            screen_arr["1"] = screen.textContent
+            screen_arr["operator"] = element.value
+        }
+
+        else if(screen_arr["1"] != null && screen_arr["operator"] != null){
+            result()   
+            screen_arr["operator"] = element.value     
+
+        }else if(screen_arr["1"] != null && screen_arr["operator"] == null) {
+            screen_arr["operator"] = element.value
+
+        }else {
+            console.log("something went wrong")
+        }
+
     
-    }else{ 
-        console.log("something went wrong")}
- })
+    })})
 
 
- ac.addEventListener("click", () =>{
+
+equal.addEventListener("click", result)              
+    
+
+
+
+ac.addEventListener("click", () =>{
     screen.textContent = 0
     screen_arr["1"] = null
     screen_arr["operator"] = null
@@ -99,8 +148,6 @@ equal.addEventListener("click", () => {               //provides result
  })
 
 
-
-
-
 let screen_arr = {"1": null, "operator": null, "2":null };
+
 console.table(screen_arr)
